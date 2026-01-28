935419f0a64937cb1988335c71e5d4429c927310
Dhanashri Dilip Mirajkar <dhanashri.mirajkar@bounteous.com>
2026-01-28 17:28:55 +0530

    Swagger Added

--- src/main/java/com/example/EmployeeManagement/DTO/ApiDto.java
+++ src/main/java/com/example/EmployeeManagement/DTO/ApiDto.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiDto {
     private int status;
     private String message;
